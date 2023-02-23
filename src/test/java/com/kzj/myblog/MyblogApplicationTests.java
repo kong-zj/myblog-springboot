98ed63488f8e160eef08c45c8278ca7626eab744
kong-zj <kongzj1341@outlook.com>
2023-02-23 22:03:52 +0800

    init, finish yml and xml

---
+++ src/test/java/com/kzj/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzj.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
